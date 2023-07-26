94629b02a41c83cbe4cd6ad97b500c4d7ce244ab
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-07-26 18:10:25 -0400

    Fixes

--- templates/js/main.js
+++ templates/js/main.js
@@ -7,3 +7,12 @@ function main(){
         window.location.href = "/"
     }
 }
+function link(link){
+    var url = window.location.href;
+    if(url == "https://1mxthxs.github.io/writers_blog/pages/authors.html" || url == "https://1mxthxs.github.io/writers_blog/pages/login.html"){
+        window.location.href = "https://1mxthxs.github.io/writers_blog/" + link;
+    }
+    else{
+        window.location.href = link;
+    }
+}
